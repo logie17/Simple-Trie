1da110d40a21e08fe3af397c9a6d7e2c803470d7
Logan Bell <loganbell@gmail.com>
2013-01-24 16:12:26 -0800

    Added some more freshness
    * The ability to add new nodes
    * The ability search
    * Wrapped it up in some unit tests for fun

--- make_trie.pl
+++ make_trie.pl
@@ -2,23 +2,54 @@
 
 use strict;
 use warnings;
+use Test::More;
+use constant '_END' => '';
+
 
 my $TRIE = { };
 
 sub make_trie {
     my @words = @_;
     for my $word ( @words ) {
-        my $node = $TRIE;
-        my @letters = $word =~ /./g;
-        # walk the trie
-        while ( scalar @letters && exists($node->{$letters[0]}) ) {
-            $node = $node->{shift(@letters)};       
-        }
-        for my $letter (@letters) {
-            $node = $node->{$letter} = {};
-        }
-        $node->{""} = undef;
+        _add_node($word);
     }
     return $TRIE;
 }
 
+sub add_to {
+    _add_node(@_);
+}
+
+sub _add_node {
+    my $node = $TRIE;
+    my @letters = (shift) =~ /./g;
+    # walk the trie
+    while ( scalar @letters && exists($node->{$letters[0]}) ) {
+        $node = $node->{shift(@letters)};       
+    }
+    for my $letter (@letters) {
+        $node = $node->{$letter} = {};
+    }
+    $node->{_END()} = undef;
+}
+
+sub find {
+    my $word = shift;
+    my $node = $TRIE;
+    $node = $node->{$_} for $word =~ /./g;
+    return exists $node->{_END()} ? 1 : 0;
+}
+
+# TODO, interesting prototype make_trie { split /./, shift } @words;
+
+my $trie = make_trie(qw(foo bar food));
+
+is_deeply $trie, {'b' => { 'a' => { 'r' => { '' => undef }}}, 'f' => { 'o' => { 'o' => { '' => undef, 'd' => { '' => undef }}}}}; 
+ok find('foo');
+ok !find ('unknown');
+
+ok ! find('baz');
+add_to('baz');
+ok find('baz');
+
+done_testing;
